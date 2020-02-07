43e5d526146817d6d85e0a04f66303c4f2262ea8
Karanpal Singh <singhkaranpal81@gmail.com>
2020-02-07 13:57:46 +0530

    Refactoring to a Custom Hook

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
@@ -6,44 +6,15 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
-import uuid from 'uuid/v4';
+import useTodoState from './hooks/useTodoState';
 
 const TodoApp = (props) => {
     const initialTodos = JSON.parse(window.localStorage.getItem("todos") || "[]");
-
-    const [todos, setTodos] = useState(initialTodos);
+    const { todos, addTodo, removeTodo, toggleTodo, editTodo } = useTodoState(initialTodos);
 
     useEffect(() => {
         window.localStorage.setItem("todos", JSON.stringify(todos));
     }, [todos]);    // the second argument tells, useEffect should be called only when todos are changed. The arguments should be specified in an array.
-
-    const addTodo = (newTodoText) => {
-        const newTodo = {
-            id : uuid(),
-            task : newTodoText,
-            completed : false
-        };
-        setTodos([...todos, newTodo]);
-    }
-
-    const removeTodo = (todoId) => {
-        const updatedTodos = todos.filter(todo => todo.id !== todoId);
-        setTodos(updatedTodos);
-    }
-
-    const toggleTodo = (todoId) => {
-        const updatedTodos = todos.map(todo => (
-            todo.id === todoId ? {...todo, completed : !todo.completed} : todo
-        ));
-        setTodos(updatedTodos);
-    }
-
-    const editTodo = (todoId, newTask) => {
-        const updatedTodos = todos.map(todo => (
-            todo.id === todoId ? {...todo, task : newTask} : todo
-        ));
-        setTodos(updatedTodos);
-    }
 
     return(
         // Paper Tag is used for the body. 
---
+++ src/hooks/useTodoState.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import uuid from 'uuid/v4';
+
+const useTodoState = (initialTodos) => {
+    const [todos, setTodos] = useState(initialTodos);
+    return {
+        todos,
+        addTodo : (newTodoText) => {
+            const newTodo = {
+                id : uuid(),
+                task : newTodoText,
+                completed : false
+            };
+            setTodos([...todos, newTodo]);
+        },
+        removeTodo : (todoId) => {
+            const updatedTodos = todos.filter(todo => todo.id !== todoId);
+            setTodos(updatedTodos);
+        },
+        toggleTodo : (todoId) => {
+            const updatedTodos = todos.map(todo => (
+                todo.id === todoId ? {...todo, completed : !todo.completed} : todo
+            ));
+            setTodos(updatedTodos);
+        },
+        editTodo : (todoId, newTask) => {
+            const updatedTodos = todos.map(todo => (
+                todo.id === todoId ? {...todo, task : newTask} : todo
+            ));
+            setTodos(updatedTodos);
+        }
+    }    
+}
+
+export default useTodoState;
